88922ce027ceaddcd1300769316a5cb7ca3da509
rjunagadhwala <rjunagadhwala@luc.edu>
2022-10-14 17:09:57 -0500

    Signupform background image

--- ItineraryApp/screens/signupform.js
+++ ItineraryApp/screens/signupform.js
@@ -1,7 +1,7 @@
 import { ImageBackground, Stylesheet, Text, View, Platform, Dimensions, TouchableOpacity, Pressable } from 'react-native';
 export default SignupForm
 
-const image = { uri: "https://unsplash.com/photos/HL6gzlxnMjM"};
+const image = { uri: "https://images.unsplash.com/photo-1492138195150-d71143066b68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"};
 
 const SignupForm = () => (
     <View style={styles.container}>
