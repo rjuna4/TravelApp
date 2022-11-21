299726d730439963bed2f4fccd5de3f9775362d2
pragyakansal <pragyakansal419@gmail.com>
2022-11-20 19:07:41 -0600

    Changes to bookmarks screen

--- ItineraryApp/screens/bookmarksscreen.js
+++ ItineraryApp/screens/bookmarksscreen.js
@@ -1,6 +1,6 @@
 import {Icon, SafeAreaView, TextInput, ImageBackground, StyleSheet, Text, View, Platform, Dimensions, TouchableOpacity, Pressable, Image, VirtualizedList, ScrollView} from 'react-native';
 import React, { Component, useState, useLayoutEffect} from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import ItineraryContainer from 'ItineraryApp/components/ItineraryContainer';
 import ActivityContainer from '../components/ActivityContainer';
 import { AsyncStorage } from '@react-native-async-storage/async-storage';
@@ -19,7 +19,7 @@ const BookmarksScreen = ({route}) => {
 
     const data = route?.params?.param
 
-    return (
+    {/*return (
     <View style={styles.container}>
       <View>
         <Text style={styles.title}>My Bookmarks</Text>
@@ -27,8 +27,54 @@ const BookmarksScreen = ({route}) => {
       <View>
         
        </View>    
-   </View>  
-    )  
+    </View> 
+    )  */}
+
+    return (
+      <>
+      <ScrollView>
+        <View style={styles.container}>
+          <Image style={styles.activityImage}
+            source={
+              {uri:
+                data?.photo?.images?.large?.url 
+                ? data?.photo?.images?.large?.url 
+                : 'ItineraryApp/assets/icons/restaurant(1).png'}
+            }
+          />
+            <View>
+              <TouchableOpacity>
+                <Image style={styles.backButton}
+                  source={require('ItineraryApp/assets/icons/Refund_back.png')}
+                 />   
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Image style={styles.addButton}
+                  source={require('ItineraryApp/assets/icons/Map_fill.png')}
+                 /> 
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image style={styles.saveButton}
+                  source={require('ItineraryApp/assets/icons/Bookmark_fill(1).png')}
+                 /> 
+              </TouchableOpacity>
+            </View>
+            <View>
+              <Text style={styles.name}>{data?.name}</Text>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Image style={{marginHorizontal: 3}}
+                  source={require('ItineraryApp/assets/icons/Pin_fill.png')}
+                />  
+                 <Text style={styles.location}>{data?.location_string}</Text>
+              </View>
+             
+            </View>
+        </View>
+      </ScrollView>  
+      </>
+    );
   }
 
 
@@ -38,8 +84,6 @@ const BookmarksScreen = ({route}) => {
       response = JSON.parse(token);
     })
   }
-
- 
 
 export default BookmarksScreen
 
@@ -55,4 +99,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 25,
   },
+  activityImage: {
+    width: '85%',
+    height: 200,
+    borderTopLeftRadius: 13,
+    borderTopRightRadius: 13,
+    marginHorizontal: 10,
+    top: 10
+  }
 });
