8577dd4c4d95f15b8c8501ab8d1a0b19f443b3bc
Mark Mingo <mmingo@luc.edu>
2022-12-05 12:56:08 -0600

    Added component for proper functionality

--- ItineraryApp/App.js
+++ ItineraryApp/App.js
@@ -7,6 +7,7 @@ import MoreInformation from './screens/moreInformation';
 import MapTest from './screens/mapsTest';
 import HomeScreen from './screens/homescreen';
 import ProfileScreen from './screens/profilescreen';
+import ProfileSettings from './screens/profileSettings';
 import BookmarksScreen from './screens/bookmarksscreen';
 import ItineraryListScreen from './screens/itinerarylistscreen';
 import ActivityRecommendations from './screens/activityrecommendations';
@@ -46,6 +47,7 @@ class App extends Component {
           <Stack.Screen name="SignupForm" component={SignupForm} options={{headerShown: false}}/>
           <Stack.Screen name="LoginForm" component={LoginForm} options={{headerShown: false}}/>
           <Stack.Screen name="ViewItinerary" component={ViewItinerary} options={{headerShown: false}}/>
+          <Stack.Screen name="ProfileSettings" component={ProfileSettings} options={{headerShown: false}}/>
           {/* <Stack.Screen name="MoreInformation" component={MoreInformation} />
           <Stack.Screen name="MapTest" component={MapTest} />
           <Stack.Screen name="HomeScreen" component={HomeScreen} />
